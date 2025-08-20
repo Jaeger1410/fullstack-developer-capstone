f27ec4133417ad6150d6c89c64374b6f4aa74547
Carlos Cortés Poblete <cortespoblete.carlos@gmail.com>
2025-08-20 14:21:02 -0400

    Update populate.py

--- server/djangoapp/populate.py
+++ server/djangoapp/populate.py
@@ -60,54 +60,53 @@ def initiate():
         "car_make": car_make_instances[2]
         },
         {
-         "car_make": 
-        ar_make_instan
-        es[2]},
-
-
-         
-                    
-         {"name": "A5"
-        ""type": "SU,
-        , "year": 2023, "car_make": car_m
-        ake_instances[2]},
+        "name": "A5",
+        "type": "SUV",
+        "year": 2023,
+        "car_make": car_make_instances[2]
+        },
+        {
+        "name": "A6"
+        "type": "SUV",
+        "year": 2023,
+        "car_make": car_make_instances[2]
+        },
         {
         "name": "Sorrento",
-        : "SUV", "year
-        : 2023, "car_
-        ake": car_make_instances[2]},
-
-         
-               {"name": "So
-        rento", "type"
-         "SUV", "year
-        : 2023, "car_make": car_make_inst
-        ances[3]},
-        {
-        "name": "Carnival
-        , "type": "SUV",
+        "type": "SUV",
         "year": 2023,
         "car_make": car_make_instances[3]
         },
         {
-        "name": "Cerato", 
-        type": "Sedan", 
-        year": 2023, 
-        car_make": car_make_instances[3]}
-        ,
+        "name": "Carnival",
+        "type": "SUV",
+        "year": 2023,
+        "car_make": car_make_instances[3]
+        },
         {
-        "name": "Corolla
-        , "type": "Sedan
-        , "year": 202
-        , "car_make": car_make_instances[
-        4]},
+        "name": "Cerato",
+        "type": "Sedan",
+        "year": 2023,
+        "car_make": car_make_instances[3]
+        },
         {
-        "name": "Camry", 
-        type": "Sedan"
-         "year": 2023
-         "car_make": car_make_instances[4
-        ]},
-      {"name": "Kluger", "type": "SUV", "year": 2023, "car_make": car_make_instances[4]},
+        "name": "Corolla",
+        "type": "Sedan",
+        "year": 2023,
+        "car_make": car_make_instances[4]
+        },
+        {
+        "name": "Camry",
+        "type": "Sedan",
+        "year": 2023,
+        "car_make": car_make_instances[4]
+        },
+        {
+        "name": "Kluger",
+        "type": "SUV",
+        "year": 2023,
+        "car_make": car_make_instances[4]
+        },
         # Add more CarModel instances as needed
     ]
 
