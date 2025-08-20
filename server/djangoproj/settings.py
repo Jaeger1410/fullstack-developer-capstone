4cd004b27eb3ba67cee132f4ed3147a17f78b9aa
Carlos Cortés Poblete <cortespoblete.carlos@gmail.com>
2025-08-20 14:21:26 -0400

    Update settings.py

--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY =\
 DEBUG = True
 
 ALLOWED_HOSTS = ['localhost',
-                 'https://cortespoblet-8000.'\
+                 'https://cortespoblet-8000.'
                  'theiadockernext-1-labs-prod-theiak8s-4-tor01.'
                  'proxy.cognitiveclass.ai']
 CSRF_TRUSTED_ORIGINS = ['https://cortespoblet-8000.'
